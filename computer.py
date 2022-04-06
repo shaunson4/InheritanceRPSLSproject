e55f57a419c4d80695e79e0cb64bd7e51a7f3be6
shaunson4 <sgray4106@gmail.com>
2022-04-06 11:56:40 -0500

    We created two separate child classes underneath the Player parent class, Human class, and Computer class

---
+++ computer.py
@@ -0,0 +1,7 @@
+from player import Player
+
+class Computer(Player):
+    def __init__(self):
+        self.name = ''
+        super().__init__()
+
---
+++ human.py
@@ -0,0 +1,8 @@
+from player import Player
+
+class Human(Player):
+    def __init__(self):
+        self.name = ''
+        super().__init__()
+
+        
