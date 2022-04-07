359a200fd1f57040e8c7db00f5dd9e2f80f41c7e
shaunson4 <sgray4106@gmail.com>
2022-04-07 14:23:42 -0500

    Added while loop on the run game method and included a conditional statement on the compare gesture method to determine the winner of each individual round.

--- game.py
+++ game.py
@@ -11,9 +11,11 @@ class Game():
         print('Welcome to a game of Rock, Paper, Scissors, Lizzard, Spock!')
         print('Rule Key is as follows: Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock ')   
         self.player_two_choice()
-        self.select_gesture()
-
-        # self.player_one.choose_gesture()
+        while self.player_one.score < 2 and self.player_two.score < 2:
+            self.select_gesture()
+            self.compare_gestures()
+            
+       
 
     def player_two_choice(self):
         opponent_choice = input('Do you want to play the computer? y/n: ')
@@ -29,6 +31,29 @@ class Game():
         self.player_one.choose_gesture()
         self.player_two.choose_gesture()
         print(f' Player Two gesture is: {self.player_two.chosen_gesture}')
+    
+    def compare_gestures(self):
+        if self.player_one.chosen_gesture == self.player_two.chosen_gesture:
+            print('Tie round, start again')
+        elif self.player_one.chosen_gesture == 'Rock' and (self.player_two.chosen_gesture == 'Scissors'or self.player_two.chosen_gesture == 'Lizard'):
+            self.player_one.score += 1
+            print('Player one wins Round!')
+        elif self.player_one.chosen_gesture == 'Scissors' and (self.player_two.chosen_gesture == 'Paper'or self.player_two.chosen_gesture == 'Lizard'):
+            self.player_one.score += 1
+            print('Player one wins Round!')
+        elif self.player_one.chosen_gesture == 'Paper' and (self.player_two.chosen_gesture == 'Rock'or self.player_two.chosen_gesture == 'Spock'):
+            self.player_one.score += 1
+            print('Player one wins Round!')
+        elif self.player_one.chosen_gesture == 'Lizard' and (self.player_two.chosen_gesture == 'Paper'or self.player_two.chosen_gesture == 'Spock'):
+            self.player_one.score += 1
+            print('Player one wins Round!')
+        elif self.player_one.chosen_gesture == 'Spock' and (self.player_two.chosen_gesture == 'Rock'or self.player_two.chosen_gesture == 'Scissors'):
+            self.player_one.score += 1
+            print('Player one wins Round!')
+        else:
+            self.player_two.score += 1
+            print('Player Two wins Round!')
+ 
         
 
 
